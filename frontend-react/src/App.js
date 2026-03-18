9de7922639de00454493b110768df23404ed77e4
Fran Rodriguez <rodriguezfrannn@gmail.com>
2026-03-17 21:06:40 -0300

    feat: add cart feedback, new pages and navbar refactor

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -9,18 +9,19 @@ import Dashboard from "./components/styles/pages/Dashboard.jsx";
 import RutaProtegida from "./components/styles/pages/RutaProtegida.jsx";
 import NotFound from "./components/NotFound.jsx";
 import ProductoDetalle from "./components/ProductoDetalle.jsx";
+import ComoComprar from "./components/ComoComprar.jsx";
+import Contacto from "./components/Contacto.jsx";
 
 import { useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./components/styles/Navbar.css";
+import toast, { Toaster } from 'react-hot-toast'
 
 
 //=== FUNCION PRINCIPAL, CONTROLA CARRITO, PETICION AL BACK Y RENDERIZA COMPONENTES===//
 function App() {
  const [carrito, setCarrito]= useState ([])
  const [isLogIn, setIsLogIn]= useState (false)
- 
-
 
   //AL MONTAR CARGA EL CARRITO GUARDADO UNA VEZ, BUSCA EL TOKEN PARA LOGGEAR Y MANDA AL NAVBAR DATOS:
   useEffect (()=>{
@@ -32,7 +33,6 @@ function App() {
     if (token){
       setIsLogIn (true)
     }
-
   },[])
 
   //FUNCION PARA MANEJAR EL LOGGOUT, ACTUALIZAR EL ESTADO Y VA COMO PROP EN NAVBAR:
@@ -55,7 +55,7 @@ function App() {
  async function addCart(productId) {  
 
   try {
-    const sendRequest= await fetch ("http://localhost:4000/api/cart/add", //Este POST llega a cart.services.js
+    const sendRequest= await fetch ("http://localhost:4000/api/cart/add",
       {
         method: "POST",
         headers: {"Content-Type" : "application/json"},
@@ -70,42 +70,49 @@ function App() {
     const data= await sendRequest.json ()
 
     //FILTRAR PRODUCTO, CREAR CANTIDAD Y SUMAR O AGREGAR POR PRIMERA VEZ
-    
-const findProdCart = carrito.find(item => item.id === data.producto.id);
+    const findProdCart = carrito.find(item => item.id === data.producto.id);
 
-if (!findProdCart) {
-  setCarrito(prevCart => [...prevCart, { ...data.producto, cantidad: 1 }]);
-} else {
-  setCarrito(prevCart =>
-    prevCart.map(item =>
-      item.id === data.producto.id
-        ? { ...item, cantidad: item.cantidad + 1 }
-        : item
-    )
-  );
-}
-
-
+    if (!findProdCart) {
+      setCarrito(prevCart => [...prevCart, { ...data.producto, cantidad: 1 }]);
+    } else {
+      setCarrito(prevCart =>
+        prevCart.map(item =>
+          item.id === data.producto.id
+            ? { ...item, cantidad: item.cantidad + 1 }
+            : item
+        )
+      );
+    }
+    toast.success("Producto agregado", {
+    style: {
+      background: '#f59e0b',
+      color: '#fff',
+      border: 'none',
+      fontWeight: 'bold',
+    }
+})
 
   } catch (error){
     console.error ("Error al enviar la peticion al Back-end")
+    toast ("Error al agregar producto")
   }
-  
  }
-    
 
-      
   return (
     <BrowserRouter>
     <div className="App">
+      <Toaster/>
       <Navbar cartCounter={carrito.length} isLogIn={isLogIn} handleLogOut={handleLogOut}/>
       
       <Routes>
       <Route path="/" element={<ListaProductos addCart={addCart}/>} />
       <Route path="/carrito" element= {<Cart fullCart={carrito} setCarrito={setCarrito}/>}/>
+      
       <Route path= "/admin/login" element= {<Admin handleLogIn={handleLogIn}/>}/>
       <Route path="/admin/dashboard" element={<RutaProtegida><Dashboard isLogIn={isLogIn}/></RutaProtegida>}/>
       <Route path="*" element={<NotFound/>}></Route>
+      <Route path="/como-comprar" element={<ComoComprar/>}/>
+      <Route path="/contacto" element={<Contacto/>}/>
       <Route path="/producto/:id" element={<ProductoDetalle addCart={addCart}/>}/>
       </Routes>
       
--- frontend-react/src/components/styles/pages/Dashboard.jsx
+++ frontend-react/src/components/styles/pages/Dashboard.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
-import { motion, AnimatePresence, number } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 import Card from "../../ProductCard";
 
 function Dashboard({isLogIn}) {
