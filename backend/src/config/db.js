8860f40c28c2a10825b7adba9adee1aa37b8c320
Fran Rodriguez <rodriguezfrannn@gmail.com>
2026-03-20 16:09:10 -0300

    feat: railway

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,13 +1,17 @@
 //===Conexion a la base de datos===//
 const mysql = require('mysql2/promise'); 
-require('dotenv').config();
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '../../.env') });
 
 const baseDatos = mysql.createPool({
     host: process.env.MYSQLHOST,
     user: process.env.MYSQLUSER,
     password: process.env.MYSQLPASSWORD,
     database: process.env.MYSQLDATABASE,
-    port: process.env.MYSQLPORT
+    port: process.env.MYSQLPORT || 3306,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
 async function testConexion() {
